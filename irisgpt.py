f646e5ac38fef04a44748c89f41528a930e92cd8
Oscar Goes <goesoscar@utexas.edu>
2023-09-03 18:47:36 -0500

    Switching Azure CV AI to SDK instead of REST

--- irisgpt.py
+++ irisgpt.py
@@ -1,19 +1,21 @@
 import speech_recognition as sr
 import pyttsx3
-import time
+from datetime import date
 import cv2 as cv
 import os
 from dotenv import load_dotenv
 import openai
-import http.client, urllib.request, urllib.parse, urllib.error, base64
+import azure.ai.vision as sdk
 
 # Initialize globals and Python TTS engine
+global frame
 engine = pyttsx3.init()
-engine.setProperty('volume', 1.0)
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+engine.setProperty("volume", 1.0)
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
 WAKE_WORD = "iris"
 LOG = "log.txt"
+IMG = "frame.jpg"
 
 # Initialize keywords for sphinx to focus on
 keywords = [("iris", 1), ("Iris", 1),]
@@ -23,16 +25,19 @@ is_listening = False
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-headers = {
-    # Request headers
-    'Content-Type': 'application/json',
-    'Ocp-Apim-Subscription-Key': os.getenv("AZURE_AI_KEY"),
-}
+# Initialize Azure Computer Vision API
+service_options = sdk.VisionServiceOptions(os.getenv("AZURE_VISION_ENDPOINT"),
+                                           os.getenv("AZURE_VISION_KEY"))
 
-params = urllib.parse.urlencode({
-    # Request parameters
-    'model-version': '{string}',
-})
+vision_source = sdk.VisionSource(filename=IMG)
+
+analysis_options = sdk.ImageAnalysisOptions()
+analysis_options.features = (
+    sdk.ImageAnalysisFeature.DENSE_CAPTIONS |
+    sdk.ImageAnalysisFeature.TEXT
+)
+analysis_options.language = "en"
+analysis_options.gender_neutral_caption = False
 
 # Wait to hear wake word and listen for command when wake detected
 def hear(rec, audio):
@@ -71,52 +76,56 @@ def listen():
 
 # Takes in text from speech and determines which command to run
 def interpret(text):
-    f = open(LOG, "a")
-    f.write("User: " + text + "\n")
-    speak = "I do not know that command yet"
+    with open(LOG, "a") as f:
+        f.write("User: " + text + "\n")
+        speak = "I do not know that command yet"
 
-    if text == "go to sleep":
-        speak = "going to sleep"
-        f.write("Iris: " + speak + "\n")
-        global is_listening
-        is_listening = False
-    elif text == "say hello":
-        speak = "hello, how are you?"
-        f.write("Iris: " + speak + "\n")
-    elif text == "how are you":
-        speak = "fine, thank you"
-        f.write("Iris: " + speak + "\n")
-    else:
-        # try:
-        #     conn = http.client.HTTPSConnection('*.cognitiveservices.azure.com')
-        #     conn.request("POST", "/computervision/retrieval:vectorizeImage?api-version=2023-04-01-preview&%s" % params, "{body}", headers)
-        #     response = conn.getresponse()
-        #     data = response.read()
-        #     print(data)
-        #     conn.close()
-        # except Exception as e:
-        #     print("[Errno {0}] {1}".format(e.errno, e.strerror))
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {
-                    "role": "user",
-                    "content": text
-                }
-            ],
-            temperature=1,
-            max_tokens=128,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-        f.write("Iris: " + response + "\n")
+        if text == "go to sleep":
+            speak = "going to sleep"
+            f.write("Iris: " + speak + "\n")
+            global is_listening
+            is_listening = False
+        elif text == "say hello":
+            speak = "hello, how are you?"
+            f.write("Iris: " + speak + "\n")
+        elif text == "how are you":
+            speak = "fine, thank you"
+            f.write("Iris: " + speak + "\n")
+        elif text == "analyze image":
+            cv.imwrite("frame.jpg", frame)
+        else:
+            # try:
+            #     conn = http.client.HTTPSConnection('*.cognitiveservices.azure.com')
+            #     conn.request("POST", "/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&%s" % params, "{body}", headers)
+            #     response = conn.getresponse()
+            #     data = response.read()
+            #     print(data)
+            #     conn.close()
+            # except Exception as e:
+            #     print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-    f.close()
-    # engine.say(speak)
-    # engine.runAndWait()
+            # response = openai.ChatCompletion.create(
+            #     model="gpt-3.5-turbo",
+            #     messages=[
+            #         {
+            #             "role": "user",
+            #             "content": text
+            #         }
+            #     ],
+            #     temperature=1,
+            #     max_tokens=128,
+            #     top_p=1,
+            #     frequency_penalty=0,
+            #     presence_penalty=0
+            # )
+            f.write("Iris: " + text + "\n")
+
+        f.close()
+        # engine.say(speak)
+        # engine.runAndWait()
 
 
+# Initialize video feed
 videoCapture = cv.VideoCapture(0, cv.CAP_DSHOW)
 videoCapture.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
 videoCapture.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
@@ -132,6 +141,13 @@ with mic as source:
 # Run hear function in a new thread to allow main function to run while listening in background
 stopper = rec.listen_in_background(mic, hear)
 is_listening = True
+
+# Time stamp onto log
+today = str(date.today())
+with open(LOG, "a") as f:
+    f.write("--- Started on: " + today + " ---\n")
+    f.close()
+
 while(True):
     ret, frame = videoCapture.read()
     if not ret:
@@ -155,3 +171,6 @@ while(True):
 
 cv.destroyAllWindows() 
 videoCapture.release()
+with open(LOG, "a") as f:
+    f.write("--- Ended conversation ---\n\n")
+    f.close()
