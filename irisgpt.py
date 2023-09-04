87dee4589c3abec9c1df012cedaf8aa874916f1b
Oscar Goes <goesoscar@utexas.edu>
2023-09-03 21:21:33 -0500

    Finished switch to Azure AI sdk. Connecting to gpt

--- irisgpt.py
+++ irisgpt.py
@@ -29,8 +29,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 service_options = sdk.VisionServiceOptions(os.getenv("AZURE_VISION_ENDPOINT"),
                                            os.getenv("AZURE_VISION_KEY"))
 
-vision_source = sdk.VisionSource(filename=IMG)
-
 analysis_options = sdk.ImageAnalysisOptions()
 analysis_options.features = (
     sdk.ImageAnalysisFeature.DENSE_CAPTIONS |
@@ -93,6 +91,44 @@ def interpret(text):
             f.write("Iris: " + speak + "\n")
         elif text == "analyze image":
             cv.imwrite("frame.jpg", frame)
+            vision_source = sdk.VisionSource(filename=IMG)
+            image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
+            result = image_analyzer.analyze()
+            if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+                print(" Image height: {}".format(result.image_height))
+                print(" Image width: {}".format(result.image_width))
+                print(" Model version: {}".format(result.model_version))
+
+                if result.dense_captions is not None:
+                    print(" Dense Captions:")
+                    for caption in result.dense_captions:
+                        print("   '{}', {}, Confidence: {:.4f}".format(caption.content, caption.bounding_box, caption.confidence))
+
+                if result.text is not None:
+                    print(" Text:")
+                    for line in result.text.lines:
+                        points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
+                        print("   Line: '{}', Bounding polygon {}".format(line.content, points_string))
+                        for word in line.words:
+                            points_string = "{" + ", ".join([str(int(point)) for point in word.bounding_polygon]) + "}"
+                            print("     Word: '{}', Bounding polygon {}, Confidence {:.4f}"
+                                .format(word.content, points_string, word.confidence))
+
+                result_details = sdk.ImageAnalysisResultDetails.from_result(result)
+                print(" Result details:")
+                print("   Image ID: {}".format(result_details.image_id))
+                print("   Result ID: {}".format(result_details.result_id))
+                print("   Connection URL: {}".format(result_details.connection_url))
+                print("   JSON result: {}".format(result_details.json_result))
+
+            else:
+
+                error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+                print(" Analysis failed.")
+                print("   Error reason: {}".format(error_details.reason))
+                print("   Error code: {}".format(error_details.error_code))
+                print("   Error message: {}".format(error_details.message))            
         else:
             # try:
             #     conn = http.client.HTTPSConnection('*.cognitiveservices.azure.com')
